0ab5a0577238f2b3b9bd5af85e4b9b2af8ff24a1
rworsham <robertworsham01@gmail.com>
2024-10-20 16:30:06 -0400

    Added react-router and some basics files. Am actively learning this and how to create an SPA

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Home from './components/Home'
+import Contact from './components/Contact'
+import Dashboard from './components/Dashboard'
+import Login from './components/Login'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/dashboard" component={Dashboard} />
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
